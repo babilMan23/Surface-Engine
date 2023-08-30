7bfc5f2c1638855680f3e2dd7293fbe1c88453ac
babilMan23 <137325479+babilMan23@users.noreply.github.com>
2023-08-30 18:52:02 +0300

    Initial Commit

---
+++ Surface/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Surface
+//
+//  Created by babilMan23 on 8/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
